dd710e70e10e0811f9b69cf9b3d512182fce597a
Nya Garcia <nyablk97@gmail.com>
2020-09-19 19:21:23 +0200

    feat(scully.rxjs-docs.config): add routes

--- scully.rxjs-docs.config.ts
+++ scully.rxjs-docs.config.ts
@@ -1,6 +1,6 @@
 import { ScullyConfig } from '@scullyio/scully';
 
-export const config: ScullyConfig = {
+export const config: Partial<ScullyConfig> = {
   projectRoot: './src',
   projectName: 'rxjs-docs',
   outDir: './dist/static',
@@ -8,8 +8,23 @@ export const config: ScullyConfig = {
     '/:page': {
       type: 'contentFolder',
       page: {
-        folder: './src/assets/doc',
+        folder: './src/assets/doc/sections',
       },
     },
+    '/concepts/:page': {
+      type: 'contentFolder',
+      page: {
+        folder: './src/assets/doc/concepts',
+      },
+    },
+    /* '/operators/:category/:page': {
+      type: 'contentFolder',
+      category: {
+        folder: './src/assets/doc/operators',
+      },
+      page: {
+        folder: './src/assets/operators/:category',
+      },
+    }, */
   },
 };
