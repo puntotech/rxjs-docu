7246a2708ac15fc9ec6526f5e8edbf112a279d1c
juanT <juaniman.2000@gmail.com>
2020-09-19 23:37:59 +0200

    refactor(app): place docs-home in app component
    This is because scully has trouble detecting the paths. It is a temporary solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { HighlightService } from './shared/services/highlight.service';
+import { faFolder } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'rxjs-docs';
+
+  constructor(
+    private library: FaIconLibrary,
+    private highlightService: HighlightService
+  ) {
+    library.addIcons(faFolder);
+  }
 }
