8da75ba46b8dd4384cb7d85402972dea33a5b352
Nya Garcia <nyablk97@gmail.com>
2020-09-21 18:10:53 +0200

    feat(home-routing.module): add root route

--- src/app/views/home/home-routing.module.ts
+++ src/app/views/home/home-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ContentComponent } from './content/content.component';
+import { HomeComponent } from './home.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'concepts/:page', component: ContentComponent },
   { path: 'operators/:category/:page', component: ContentComponent },
   { path: ':page', component: ContentComponent },
