fad5a0b63261bcc8a7e3e41873adbd9634b86bee
Nya Garcia <nyablk97@gmail.com>
2020-09-21 18:06:38 +0200

    feat(services): add sidenav service

---
+++ src/app/shared/services/sidenav.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+
+@Injectable({ providedIn: 'root' })
+export class SidenavService {
+  private sidenav: MatSidenav;
+
+  set(sidenav: MatSidenav) {
+    this.sidenav = sidenav;
+  }
+
+  public open() {
+    return this.sidenav.open();
+  }
+
+  public close() {
+    return this.sidenav.close();
+  }
+
+  public toggle(): void {
+    this.sidenav.toggle();
+  }
+}
