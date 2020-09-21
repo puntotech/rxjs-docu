f31fe5b4ae4d1ab44549ed2884a63cc8d80370a2
Nya Garcia <nyablk97@gmail.com>
2020-09-21 18:14:37 +0200

    feat(content.component.ts): add highlight logic and remove useless code

--- src/app/views/home/content/content.component.ts
+++ src/app/views/home/content/content.component.ts
@@ -1,8 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { map } from 'rxjs/operators';
-import { faFolder } from '@fortawesome/free-solid-svg-icons';
-import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { Component, AfterViewChecked } from '@angular/core';
 import { HighlightService } from 'src/app/shared/services/highlight.service';
 
 @Component({
@@ -10,31 +6,10 @@ import { HighlightService } from 'src/app/shared/services/highlight.service';
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css'],
 })
-export class ContentComponent implements OnInit {
-  page: string;
-  defaultPage: string = 'introducción';
+export class ContentComponent implements AfterViewChecked {
+  constructor(private highlightService: HighlightService) {}
 
-  constructor(
-    private route: ActivatedRoute,
-    private library: FaIconLibrary,
-    private highlightService: HighlightService
-  ) {
-    library.addIcons(faFolder);
-  }
-
-  ngOnInit(): void {
-    /* this.route.params
-      .pipe(map((params) => params?.page || this.defaultPage))
-      .subscribe((page) => {
-        console.log(page);
-        this.page = `assets/${page}.md`;
-      }); */
-
-    this.route.url
-      .pipe(map((url) => url.map(({ path }) => path).join('/')))
-      .subscribe((url) => {
-        console.log(url);
-        this.page = `assets/doc/${url}.md`;
-      });
+  ngAfterViewChecked(): void {
+    this.highlightService.highlightAll();
   }
 }
