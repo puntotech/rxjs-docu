98274d1afed1a2ee409c5aa77d19511ad22ffed3
Nya Garcia <nyablk97@gmail.com>
2020-09-27 19:29:40 +0200

    feat(app.component): add sidenav open/close logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,46 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, HostListener, OnInit } from '@angular/core';
 import { SidenavService } from '@shared/services/sidenav.service';
 import { MatSidenav } from '@angular/material/sidenav';
+import { filter } from 'rxjs/operators';
+
+import { Router, NavigationEnd } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  @ViewChild(MatSidenav) public sidenav: MatSidenav;
+export class AppComponent implements OnInit {
+  @ViewChild(MatSidenav, { static: true }) sidenav: MatSidenav;
 
-  constructor(private sidenavService: SidenavService) {}
+  maxMobileDisplayWidth = 1000;
+  isMobileDisplay: boolean;
+
+  constructor(private sidenavService: SidenavService, private router: Router) {}
+
+  ngOnInit() {
+    this.isMobileDisplay = this.isMobileDisplayWidth(window.innerWidth);
+    this.updateSidenav();
+
+    this.router.events
+      .pipe(filter((val) => val instanceof NavigationEnd))
+      .subscribe((_) => this.updateSidenav());
+  }
 
   ngAfterViewInit(): void {
     this.sidenavService.set(this.sidenav);
+  }
+
+  @HostListener('window:resize', ['$event.target.innerWidth'])
+  onResize(width: number) {
+    this.isMobileDisplay = this.isMobileDisplayWidth(width);
+    this.updateSidenav();
+  }
+
+  private updateSidenav() {
+    this.sidenav.toggle(!this.isMobileDisplay);
+  }
+
+  private isMobileDisplayWidth(width: number) {
+    return width < this.maxMobileDisplayWidth;
   }
 }
