87076ac4fa17d81149c4b4f35384c734a2fa741c
tonivj5 <tonivj5@gmail.com>
2020-10-07 19:01:59 +0200

    fix: use scully plugin to rewrite base-href

--- scully.rxjs-docs.config.ts
+++ scully.rxjs-docs.config.ts
@@ -1,9 +1,12 @@
-import { ScullyConfig } from '@scullyio/scully';
+import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
+import { baseHrefRewrite } from '@scullyio/scully-plugin-base-href-rewrite';
 
+setPluginConfig(baseHrefRewrite, { href: '/rxjs-docu/' });
 export const config: Partial<ScullyConfig> = {
   projectRoot: './src',
   projectName: 'rxjs-docs',
   outDir: './dist/static',
+  defaultPostRenderers: [baseHrefRewrite],
   routes: {
     '/:page': {
       type: 'contentFolder',
