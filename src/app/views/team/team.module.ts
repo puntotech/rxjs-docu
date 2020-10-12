0e5666bb931858904fa71da00e3492443137fdf2
Nya Garcia <nyablk97@gmail.com>
2020-10-12 15:31:00 +0200

    feat(team.module): import FlexLayoutModule

--- src/app/views/team/team.module.ts
+++ src/app/views/team/team.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { TeamComponent } from './team.component';
 import { TeamRoutingModule } from './team-routing.module';
 import { TeamListComponent } from './team-list.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [TeamComponent, TeamListComponent],
-  imports: [CommonModule, TeamRoutingModule],
+  imports: [CommonModule, TeamRoutingModule, FlexLayoutModule],
 })
 export class TeamModule {}
