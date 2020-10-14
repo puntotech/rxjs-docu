8a1b542617e44d86493e65ce1045bf4280141511
Nya Garcia <nyablk97@gmail.com>
2020-10-14 15:39:30 +0200

    feat(multicast.md): add examples

--- scully.rxjs-docs.config.ts
+++ scully.rxjs-docs.config.ts
@@ -1,12 +1,12 @@
 import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
 import { baseHrefRewrite } from '@scullyio/scully-plugin-base-href-rewrite';
 
-setPluginConfig(baseHrefRewrite, { href: '/rxjs-docu/' });
+/* setPluginConfig(baseHrefRewrite, { href: '/rxjs-docu/' }); */
 export const config: Partial<ScullyConfig> = {
   projectRoot: './src',
   projectName: 'rxjs-docs',
   outDir: './dist/static',
-  defaultPostRenderers: [baseHrefRewrite],
+  /* defaultPostRenderers: [baseHrefRewrite], */
   routes: {
     '/:page': {
       type: 'contentFolder',
